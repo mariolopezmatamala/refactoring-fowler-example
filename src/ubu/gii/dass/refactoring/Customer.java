af3950ecacefa2c8967daa327158d9af8c385718
mariolopezmatamala <mariomatamala12@gmail.com>
2024-05-10 11:33:22 +0200

    Remove temp field -  totalAmount & thisAmount in #1

--- src/ubu/gii/dass/refactoring/Customer.java
+++ src/ubu/gii/dass/refactoring/Customer.java
@@ -31,25 +31,37 @@ public class Customer {
 	};
 
 	public String statement() {
-		double totalAmount = 0;
 		int totalFrequentRenterPoints = 0;
 		Iterator<Rental> rentals = _rentals.iterator();
 		String result = "Rental Record for " + getName() + "\n";
 		while (rentals.hasNext()) {
-			double thisAmount = 0;
+			
 			Rental each = rentals.next();
-			thisAmount = each.getCharge();
 			
 			totalFrequentRenterPoints += each.getFrequentRenterPoints();
 			// show figures for this rental
 			result += "\t" + each.getMovie().getTitle() + "\t"
-					+ String.valueOf(thisAmount) + "\n";
-			totalAmount += thisAmount;
+					+ String.valueOf(each.getCharge()) + "\n";
+
 		}
 		// add footer lines
-		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
+		result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
 		result += "You earned " + String.valueOf(totalFrequentRenterPoints)
 				+ " frequent renter points";
 		return result;
+	}
+	
+	private double getTotalCharge(){
+		double result = 0;
+
+		Iterator<Rental> rentals = _rentals.iterator();
+		while (rentals.hasNext()) {
+
+			Rental each = rentals.next();
+			result += each.getCharge();
+		}
+
+		return result;
+
 	}
 }
