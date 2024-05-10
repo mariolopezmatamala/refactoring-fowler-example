1134916f441eeae2caf0f0ad0dd9edb3ec303560
mariolopezmatamala <mariomatamala12@gmail.com>
2024-05-10 10:52:01 +0200

    Extract rename amountFor - getCharge in #1

--- src/ubu/gii/dass/refactoring/Customer.java
+++ src/ubu/gii/dass/refactoring/Customer.java
@@ -38,7 +38,7 @@ public class Customer {
 		while (rentals.hasNext()) {
 			double thisAmount = 0;
 			Rental each = rentals.next();
-			thisAmount = amountFor(each);
+			thisAmount = getCharge(each);
 			
 			// add frequent renter points
 			frequentRenterPoints++;
@@ -58,24 +58,24 @@ public class Customer {
 		return result;
 	}
 
-	private double amountFor(Rental each) {
-		double thisAmount = 0;
+	private double getCharge(Rental aRental) {
+		double result = 0;
 		// determine amounts for each line
-		switch (each.getMovie().getPriceCode()) {
+		switch (aRental.getMovie().getPriceCode()) {
 		case Movie.REGULAR:
-			thisAmount += 2;
-			if (each.getDaysRented() > 2)
-				thisAmount += (each.getDaysRented() - 2) * 1.5;
+			result += 2;
+			if (aRental.getDaysRented() > 2)
+				result += (aRental.getDaysRented() - 2) * 1.5;
 			break;
 		case Movie.NEW_RELEASE:
-			thisAmount += each.getDaysRented() * 3;
+			result += aRental.getDaysRented() * 3;
 			break;
 		case Movie.CHILDRENS:
-			thisAmount += 1.5;
-			if (each.getDaysRented() > 3)
-				thisAmount += (each.getDaysRented() - 3) * 1.5;
+			result += 1.5;
+			if (aRental.getDaysRented() > 3)
+				result += (aRental.getDaysRented() - 3) * 1.5;
 			break;
 		}
-		return thisAmount;
+		return result;
 	}
 }
